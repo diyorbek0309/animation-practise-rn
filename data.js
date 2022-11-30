dd1da296306ff2d3475f208eeeb1002d2aefa14d
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2022-11-30 13:00:27 +0500

    changed images

--- data.js
+++ data.js
@@ -1,44 +1,39 @@
-import image from "./assets/image1.jpg";
+import image1 from "./assets/image1.jpg";
+import image2 from "./assets/image2.jpg";
+import image3 from "./assets/image3.jpg";
+import image4 from "./assets/image4.jpg";
+import image5 from "./assets/image5.jpg";
+import image6 from "./assets/image6.jpg";
 
 export const DATA = [
   {
     id: 1,
     text: "Card #1",
-    uri: image,
+    uri: image1,
   },
   {
     id: 2,
     text: "Card #2",
-    uri: image,
+    uri: image2,
   },
   {
     id: 3,
     text: "Card #3",
-    uri: image,
+    uri: image3,
   },
   {
     id: 4,
     text: "Card #4",
-    uri: image,
+    uri: image4,
   },
   {
     id: 5,
     text: "Card #5",
-    uri: image,
+    uri: image5,
   },
   {
     id: 6,
     text: "Card #6",
-    uri: image,
-  },
-  {
-    id: 7,
-    text: "Card #7",
-    uri: image,
-  },
-  {
-    id: 8,
-    text: "Card #8",
-    uri: image,
+    uri: image6,
   },
 ];
